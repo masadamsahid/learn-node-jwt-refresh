4c9b4cfb172479c58bb443e82580d5b2e16f5922
Maulana Adam Sahid <masadamsahid@gmail.com>
2022-12-11 13:24:48 +0700

    finish basic JWT

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+const jwt = require('jsonwebtoken');
 
-app.use(express.urlencoded());
+require('dotenv').config();
+
+app.use(express.json());
 
 const posts = [
   {
@@ -23,9 +26,32 @@ const posts = [
   },
 ];
 
-app.get('/posts', (req, res, next) => {
-  res.json(posts);
+app.get('/posts', authenticateToken, (req, res, next) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
+
+app.post('/login', (req, res) => {
+  // Authenticate user
+  
+  const username = req.body.username;
+  const user = { name: username };
+  
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+  
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000, () => {
   console.log(`Server is up on port $${3000}`);
